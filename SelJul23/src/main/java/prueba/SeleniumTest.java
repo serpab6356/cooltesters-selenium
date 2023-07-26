d8999ce86fa83e086d84fc108996de53f1a84b7b
sergiosarabia-abinbev <sergio.sarabia@ab-inbev.com>
2023-07-26 14:52:28 -0600

    Primer commit

---
+++ SelJul23/src/main/java/prueba/SeleniumTest.java
@@ -0,0 +1,19 @@
+package prueba;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.*;
+
+public class SeleniumTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Cool testers");
+		String exePath = "/Users/macbookpro/eclipse-workspace/SelJul23/driver/chromedriver";
+		////System.setProperty("webdriver.chrome.driver", exePath);
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://google.com");
+
+	}
+
+}
---
+++ SelJul23/src/test/java/prueba/NewTest.java
@@ -0,0 +1,17 @@
+package prueba;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.*;
+import org.testng.annotations.Test;
+
+public class NewTest {
+	@Test
+	public void f() {
+		System.out.println("Cool testers");
+		String exePath = "/Users/macbookpro/eclipse-workspace/SelJul23/driver/chromedriver";
+		//// System.setProperty("webdriver.chrome.driver", exePath);
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://google.com");
+	}
+}
